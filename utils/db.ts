51658f53c1b91650c4038ec2f439e023522f244a
77taibai <86600901+77taibai@users.noreply.github.com>
2024-06-23 15:17:20 +0800

    2024-06-23-15-17-dev

--- utils/db.ts
+++ utils/db.ts
@@ -68,32 +68,34 @@ export const initialSettings = {
 export type Settings = typeof initialSettings
 
 export const uniModals: Model[] = [
-    {
-        id: 'gemini-1.5-flash',
-        name: 'Gemini 1.5 flash',
-        provider: 'google',
-        type: 'universal'
-    },
-    {
-        id: 'gemini-1.5-pro',
-        name: 'Gemini 1.5 Pro',
-        provider: 'google',
-        type: 'universal'
-    }
+    // {
+    //     id: 'gemini-1.5-flash',
+    //     name: 'Gemini 1.5 flash',
+    //     provider: 'google',
+    //     type: 'universal'
+    // },
+    // {
+    //     id: 'gemini-1.5-pro',
+    //     name: 'Gemini 1.5 Pro',
+    //     provider: 'google',
+    //     type: 'universal'
+    // }
 ]
 
-export const textGenModels: Model[] = [{
-    id: 'gpt-3.5-turbo',
-    name: 'ChatGPT-3.5-turbo',
-    provider: 'openai',
-    endpoint: 'chat/completions',
-    type: 'chat'
-}, {
+export const textGenModels: Model[] = [
+//     {
+//     id: 'gpt-3.5-turbo',
+//     name: 'ChatGPT-3.5-turbo',
+//     provider: 'openai',
+//     endpoint: 'chat/completions',
+//     type: 'chat'
+// },
+    {
     id: '@cf/qwen/qwen1.5-14b-chat-awq',
     name: 'qwen1.5-14b-chat-awq',
     provider: 'workers-ai',
     type: 'chat'
-}, {
+},{
     id: '@cf/openchat/openchat-3.5-0106',
     name: 'openchat-3.5-0106',
     provider: 'workers-ai',
